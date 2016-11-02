ff045bd4ec60f9f0fb015824cc6b82b594be327b
bthrasher0815 <brian.thrasher@bocbats.gcsu.edu>
2016-11-02 10:12:02 -0400

    test file

--- roles/school-application-form.php
+++ roles/school-application-form.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html>
+<!--test comment-->
 <head>
     <title>School Account Information</title>
     <script src="http://ajax.googleapis.com/ajax/libs/angularjs/1.4.8/angular.min.js"></script>
@@ -9,7 +10,7 @@
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-    <link rel="stylesheet" type="text/css" href="styleSheet.css">
+    <link rel="stylesheet" type="text/css" href="css/rolesStyleSheet.css">
    
 </head>
 
