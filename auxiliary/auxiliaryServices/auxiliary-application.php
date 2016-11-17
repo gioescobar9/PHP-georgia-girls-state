4397ba5902031228a2944fb885d2381b481cac72
gioescobar9 <giovanni.escobar@bobcats.gcsu.edu>
2016-11-17 16:38:16 -0500

    finished the auxiliary-application.php file reminder, we need to run a check to see if the application is complete after every update

--- auxiliary/auxiliaryServices/auxiliary-application.php
+++ auxiliary/auxiliaryServices/auxiliary-application.php
@@ -1,18 +1,10 @@
 <?php
 session_start();
 require_once 'auxiliaryConnectDB.php';
-/*
-$id = null;
-if(!empty($_GET['id'])){
-	$id = $_REQUEST['id'];
-}
-if($id == null){
-	header("location: ../auxiliary-main-interface.php");
-}
-*/
 
-echo $_POST["appID"];
+
 $auxConnection = connectAuxDB();
+$appID = $_POST["appID"];
 $post_data = array(
 	'unitNumber' => $_POST['unitNumber'],
 	'unitCounty' => $_POST['unitCounty'],
@@ -25,8 +17,37 @@ $post_data = array(
 	'officialEmail' => $_POST['officialEmail']
 );
 
-echo "it worked";
+//convert the post_data associative array into a string in order to send it to the database
+$toInsert = implode(',',array_map(
+	function($v,$k){
+		if(is_array($v)){
+			return $k.'[]='.implode('&'.$k.'[]=', $v);
+		}else{
+			return $k.'='.$v;
+		}
+	},
+	$post_data,
+	array_keys($post_data)
+	));
+//send our application data to the database and set its status as complete
+$query = "UPDATE applications SET auxiliaryInfo = '$toInsert', auxInfoComplete = '1' WHERE applicationID = 'appID';";
+$result = $auxConnection->query($query);
+if(!$result) die("query1 failed".$auxConnection->error);
 
+/*
+now that we have submitted this applcation info we need to check if all other forms are now compete in order to update the complete field letting us know that all other applications have been submitted
+*/
+
+$query = "SELECT auxInfoComplete,schoolInfoComplete, studentInfoComplete, parentConsentInfoComplete FROM applications WHERE applicationID = '$appID';";
+$result = $auxConnection->query($query);
+if(!$result) die ("query2 failed".$auxConnection->error);
+
+$record = $result->fetch_assoc();
+if($record['auxInfoComplete'] == TRUE && $record['schoolInfoComplete'] == TRUE && $record['studentInfoComplete'] == TRUE && $record['parentConsentInfoComplete'] == TRUE){
+	$query = "UPDATE applications SET complete = '1' WHERE applicationID = '$appID';";
+	$result = $auxConnection->query($query);
+	if(!$result) die ("query3 failed".$auxConnection->error);
+}
 
 
 ?>
