e903933aa0f8f893f6c1bf831d74a9201f36548c
gioescobar9 <giovanni.escobar@bobcats.gcsu.edu>
2016-11-21 12:10:03 -0500

    fixed something

--- auxiliary/auxiliaryServices/auxiliary-application.php
+++ auxiliary/auxiliaryServices/auxiliary-application.php
@@ -5,10 +5,6 @@ require_once 'auxiliaryConnectDB.php';
 
 $auxConnection = connectAuxDB();
 
-if(isset($_POST['studID'])
-	$studID
-	$appID = $_POST["appID"];
-
 $post_data = array(
 	'unitNumber' => $_POST['unitNumber'],
 	'unitCounty' => $_POST['unitCounty'],
@@ -36,27 +32,56 @@ $toInsert = implode(',',array_map(
 	$post_data,
 	array_keys($post_data)
 	));
-//send our application data to the database and set its status as complete
-$query = "UPDATE applications SET auxInfo = '$toInsert', auxInfoComplete = '1' WHERE applicationID = '$appID';";
-$result = $auxConnection->query($query);
-if(!$result) die("query1 failed".$auxConnection->error);
 
+//send our application data to the database and set its status as complete
+if(isset($_POST['studID'])) {
+	echo "studID";
+	$studID = $_POST['studID'];
+	$query = "UPDATE applications SET auxInfo = '$toInsert', auxInfoComplete = '1' WHERE studentID = '$studID';";
+	$result = $auxConnection->query($query);
+	if(!$result) die ("query1 failed".$auxConnection->error);
+
+	/*
+now that we have submitted this applcation info we need to check if all other forms are now compete in order to update the complete field letting us know that all other applications have been submitted
+*/
+$query = "SELECT auxInfoComplete,schoolInfoComplete, studentInfoComplete, parentConsentInfoComplete FROM applications WHERE studentID = '$studID';";
+$result = $auxConnection->query($query);
+	if(!$result) die ("query3 failed".$auxConnection->error);
+	if($result->num_rows > 0){
+		$record = $result->fetch_assoc();
+		if($record['auxInfoComplete'] == TRUE && $record['schoolInfoComplete'] == TRUE && $record['studentInfoComplete'] == TRUE && $record['parentConsentInfoComplete'] == TRUE){
+		$query = "UPDATE applications SET complete = '1' WHERE applicationID = '$appID';";
+		$result = $auxConnection->query($query);
+		if(!$result) die ("query4 failed".$auxConnection->error);
+		}
+
+	}
+}
+else{
+	echo "appID";
+	$appID = $_POST["appID"];
+	$query = "UPDATE applications SET auxInfo = '$toInsert', auxInfoComplete = '1' WHERE applicationID = '$appID';";
+	$result = $auxConnection->query($query);
+	if(!$result) die("query6 failed".$auxConnection->error);
 /*
 now that we have submitted this applcation info we need to check if all other forms are now compete in order to update the complete field letting us know that all other applications have been submitted
 */
-
 $query = "SELECT auxInfoComplete,schoolInfoComplete, studentInfoComplete, parentConsentInfoComplete FROM applications WHERE applicationID = '$appID';";
 $result = $auxConnection->query($query);
-if(!$result) die ("query2 failed".$auxConnection->error);
-if($result->num_rows > 0){
-	$record = $result->fetch_assoc();
-	if($record['auxInfoComplete'] == TRUE && $record['schoolInfoComplete'] == TRUE && $record['studentInfoComplete'] == TRUE && $record['parentConsentInfoComplete'] == TRUE){
-	$query = "UPDATE applications SET complete = '1' WHERE applicationID = '$appID';";
-	$result = $auxConnection->query($query);
-	if(!$result) die ("query3 failed".$auxConnection->error);
+	if(!$result) die ("query5 failed".$auxConnection->error);
+	if($result->num_rows > 0){
+		$record = $result->fetch_assoc();
+		if($record['auxInfoComplete'] == TRUE && $record['schoolInfoComplete'] == TRUE && $record['studentInfoComplete'] == TRUE && $record['parentConsentInfoComplete'] == TRUE){
+		$query = "UPDATE applications SET complete = '1' WHERE applicationID = '$appID';";
+		$result = $auxConnection->query($query);
+		if(!$result) die ("query7 failed".$auxConnection->error);
+		}
+
+	}
 }
 
-}
+
+
 
 header('location: ../auxiliary-main-interface.php');
 
